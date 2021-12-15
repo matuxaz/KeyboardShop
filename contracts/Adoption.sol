4c72ceef7ae495a1d849fe43bd2ce526623fbabf
matuxaz <matolaiskai@gmail.com>
2021-12-15 22:02:37 +0200

    Before ruining

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -4,20 +4,37 @@ pragma solidity 0.8.10;
 contract Adoption {
 
   struct Item{
-    uint id;
     string name;
     string picture;
     string keycaps;
     string ffactor;
     string switches;
-    address[16] owner;
   }
-
+uint public keyboardCount = 0;
+uint public keyboardId = 0;
+uint[] public arrayForItems;
 address[16] public adopters;
+
+mapping(uint => Item) public keyboards;
+mapping(uint => address) public owners;
+
+constructor(){
+  add("ID80","images/ID80.png","Laser","75%","BOBA U4T");
+  add("The Classic","images/Classic.png","Botanical","65%","Cherry MX Brown");
+  add("Candy Bar","images/CandyBar.png","VileBloom","50%","BOBA Gum");
+  add("Halo","images/CandyBar.png","Halo","96%","Gateron Yellow");
+}
+
+function add(string memory name, string memory picture, string memory keycaps, string memory ffactor, string memory switches) private{
+  keyboards[keyboardId] = Item(name, picture, keycaps, ffactor, switches);
+  owners[keyboardId] = address(0);
+  keyboardId++;
+  keyboardCount++;
+}
 
 // Adopting a pet
 function adopt(uint petId) public returns (uint) {
-  require(petId >= 0 && petId <= 15);
+  require(petId >= 0 && petId <= keyboardCount);
 
   adopters[petId] = msg.sender;
 
